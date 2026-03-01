59995ed1c33af9b026c5ce855fdd66d266c8784e
jeremyauguste <jeremy.augu@gmail.com>
2026-02-28 21:33:52 -0500

    • Added some styling to the personalProfile page and created a deleteAccount button. (needs better styling and actual component functionality...)

---
+++ src/components/DeleteAccount.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Button from './ui/Button'
+
+function DeleteAccount() {
+  return (
+    <div>
+      <Button text='Delete Account' variant='delete'/>
+    </div>
+  )
+}
+
+export default DeleteAccount
